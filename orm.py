1771accd91492c856d234f12dc7d95d9c4c9beeb
= <=>
2024-01-29 06:32:51 +0100

    4,5 y6

--- orm.py
+++ orm.py
@@ -1,24 +1,43 @@
 import tkinter as tk
+import random
 
+personas = []
+numeropersonas = 20
 class Persona:
     def __init__(self):
-        self.posx = 512
-        self.posy = 512
+        self.posx = random.randint (0,1024)
+        self.posy = random.randint (0,1024)
         self.radio = 30
+        self.direccion = 0
+        self.color = "blue"
+        self.entidad = ""
     def dibuja(self):
-        lienzo.create_oval(
+        self.identidad = lienzo.create_oval(
             self.posx-self.radio/2,
             self.posy-self.radio/2,
             self.posx+self.radio/2,
             self.posy+self.radio/2,
-            fill="red")
-        
+            fill=self.color)
+    def mueve(self):
+        lienzo.move(self.entidad,5,0)
+# Creo una ventana
 raiz = tk.Tk()
-
+# En la ventana creo un lienzo
 lienzo = tk.Canvas(width=1024,height=1024)
 lienzo.pack()
-
-persona = Persona()
-persona.dibuja()
+# en la coleccion introduzco instncias de personas
+for i in range(0,numeropersonas):
+    personas.append(Persona())
+# Para cada una de las personas en la coleccion las pinto
+for persona in personas:
+    persona.dibuja()
+# Creo un bucle repetitivo    
+def bucle():
+# Para cada persona en la coleccion la muevo
+    for persona in personas:   
+        persona.mueve()
+    raiz.after(1000,bucle)
+# Ejecuto el bucle
+bucle()
 
 raiz.mainloop()
