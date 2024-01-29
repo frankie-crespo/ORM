51c50d7c2a5886a957302b37897f66020701b928
= <=>
2024-01-29 06:06:37 +0100

    2 y 3

--- orm.py
+++ orm.py
@@ -1,8 +1,24 @@
 import tkinter as tk
 
+class Persona:
+    def __init__(self):
+        self.posx = 512
+        self.posy = 512
+        self.radio = 30
+    def dibuja(self):
+        lienzo.create_oval(
+            self.posx-self.radio/2,
+            self.posy-self.radio/2,
+            self.posx+self.radio/2,
+            self.posy+self.radio/2,
+            fill="red")
+        
 raiz = tk.Tk()
 
 lienzo = tk.Canvas(width=1024,height=1024)
 lienzo.pack()
+
+persona = Persona()
+persona.dibuja()
 
 raiz.mainloop()
